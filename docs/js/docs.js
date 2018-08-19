dd928bf4d080e438818e913e6ec70a4e07efa377
Gao Wang <gaow@uchicago.edu>
2018-08-18 19:38:46 -0500

    Upload website

---
+++ docs/js/docs.js
@@ -0,0 +1,3 @@
+var reportArray = ['index']
+var reportDict = {'Performance-of-annotation-enhanced-fine-mapping-1': 'report'}
+var reportArrayMap = {'report': 'Performance of annotati ... fine-mapping'}
